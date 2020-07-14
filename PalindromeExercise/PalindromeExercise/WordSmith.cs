0db23183c18dbd9b694a616d93395fbf903ca057
Montather Handal <ali.louy2005@gmail.com>
2020-07-14 15:46:43 -0500

    Finsihed and only compare with lowercase letters
    pwd
    ]
    :q

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string str)
+        {
+            string reversedWord = ReverseString(str);
+            if(reversedWord.ToLower() != str.ToLower())
+            {
+                return false;
+            }
+            return true;
+        }
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,38 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("maomom", false)]
+
+        public void IsAPalindromeTest(string str, bool expected)
+        {
+            //Arrange
+            PalindromeExercise.WordSmith sut = new PalindromeExercise.WordSmith();
+
+            //Act
+
+            bool actual = sut.IsAPalindrome(str);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
+//DONE ake sure the PalindromeExerciseTests project file has PalindromeExercise added as a reference.
+
+//DONE → Right click dependencies in PalindromeExerciseTests to check and add if need be
+
+//Create a class named WordSmith in your PalindromeExercise project file.This is where you will eventually create your IsAPalindrome() method
+
+//In your WordSmith class, you will create a method(IsAPalindrome) that will take a parameter of type string and return a value of type bool.  If the given input is a palindrome – return true, else return false. 
+
+//Write you test method in the UnitTest1.cs file - located in the PalindromeExerciseTests project file
+
+//Remove the [Fact] portion and replace it with:
+//[Theory]
+//[InlineData()] ← you will need to put some test data and expected result here.  
+
+//For example [InlineData(“racecar”, true)]
+
+//Finish the test with arrange, act, assert and make your IsAPalindrome method pass the tests
